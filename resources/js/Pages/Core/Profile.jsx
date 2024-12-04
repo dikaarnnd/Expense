ae62fca4a0edc75d9d685c7ae87b7dcff6f76297
UTOPi4T <wsense911@gmail.com>
2024-12-04 14:28:13 +0700

    Return soon to develop

--- resources/js/Pages/Core/Profile.jsx
+++ resources/js/Pages/Core/Profile.jsx
@@ -1,9 +1,24 @@
 /* eslint-disable prettier/prettier */
+import { Head, Link } from '@inertiajs/react';
+import { useState } from 'react';
+
+import DrawerLayout from '@/Layouts/DrawerLayout';
+
 const Profile = () => {
     return (
-        <div className="profile">
-            <h1> Profile page </h1>
-        </div>
+      <>
+        <Head title="Profile" />
+        <DrawerLayout>
+          <main className="p-6 min-h-svh space-y-4">
+              <div className='mb-4 mt-2 flex items-center justify-between'>
+                <h1 className="text-2xl ">Your profile</h1>
+              </div>
+          </main>
+
+        </DrawerLayout>
+      
+      
+      </>
     );
 };
 
